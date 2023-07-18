faf5a6d444d8681fc77072b6840e6ff4e9f4169e
kimdohyung <pdohyung@gmail.com>
2023-07-18 22:49:21 +0900

    feat: 스프링 시큐리티 인증, 인가 구현을 위한 프로젝트 생성, 환경설정 및 Controller 구현

---
+++ spring-security/src/main/java/springsecurity/springsecurity/controller/UserController.java
@@ -0,0 +1,15 @@
+package springsecurity.springsecurity.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+	@PostMapping("/login")
+	public ResponseEntity<String> login(){
+		return ResponseEntity.ok().body("token");
+	}
+}
